99e931b88f116ee8b394b07371f53cbe5370939e
thakurutkarsh22 <thakurutkarsh22@gmail.com>
2024-03-25 21:23:19 +0530

    session 10

---
+++ Session10/script.js
@@ -0,0 +1,57 @@
+//  Directory -> Folder
+
+// pwd -> print working Directory
+// ls -> list down the files and folders
+// cd -> change Directory (change folder)
+// cd .. -> goes 1 level back.
+// cd ~ -> take to USer HOME Directory (/Users/uthkarshthakur)
+// cd / -> take me to the ROOT (litrally START OF EVERYTHING).
+
+// clear -> clears the terminal.
+
+// ---------- ARGUMENTS VS OPTIONS -------
+
+// optins
+
+// ls -a -> list down all the files and folders (HIDDEN FILES AND FOLDERS ALO)
+
+// if you want to see all the option of ANY command
+// man <command>
+// man ls
+
+// agument
+
+// cd /User/utkasrh
+
+// --------- TRICK ------------
+// tab -> to autocomple the command.
+// history -> up key and down key
+
+//  -------------- Absolute path and Relative apth -------------------
+
+// Relative path
+// from CURRENT PATH i want to go where
+// cd .. (from current path you want to go back)
+// cd Sqilco-APi (from current path you want to forward to Sqilco-APi )
+
+// Absolute path
+// your path should start from '/'
+
+// cd /Users;
+
+// --------------- CRUD ON FILES -----------
+// mkdir -> create new folder
+// touch filename.extension -> create a file with extension .
+// cat filename.extension  -> reads the file
+// vim filename.extension -> edits the file
+// rm filename.extension -> remove the file
+
+// TO DELETE FOLDER
+
+// rmdir folderName -> remove directory (ONLY EMPTY FOLDERS)
+// rm -r folderName -> remove directory (can delete any Derectory (filled or empty))
+
+// Copy and paste
+
+// cp sourcePath DestinationPath -> copies from source to destination.
+// mv sourcePath DestinationPath -> MOVES from source to destination.
